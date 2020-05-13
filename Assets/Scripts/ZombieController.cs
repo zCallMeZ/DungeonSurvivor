ae5f7e4a964f214b2cad2f09a2d76d5447ee1236
Solange <sol.sch1990@gmail.com>
2020-05-13 15:26:03 +0200

    Added tilemap wall and ground in scene Solange for test AI zombie + Test script ZombieController with statemachine

---
+++ Assets/Scripts/ZombieController.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ZombieController : MonoBehaviour
+{
+    void Start()
+    {
+        
+    }
+
+    enum State
+    {
+        PATROLL,
+        SEARCH,
+        ATTACK,
+        DEATH
+    }
+
+    State state = State.PATROLL;
+
+    private void Update()
+    {
+        
+        switch (state)
+        {
+            case State.PATROLL:
+
+                break;
+
+            case State.SEARCH:
+
+                break;
+
+            case State.ATTACK:
+
+                break;
+
+            case State.DEATH:
+                Destroy(gameObject);
+                break;
+        }
+
+    }
+}

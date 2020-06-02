4e072ecfe8d1bf7998dd35bdebae8889a052621a
Bryan <bryancrim@gmail.com>
2020-06-02 14:25:32 +0200

    PlayerHealth1 Scripts added

---
+++ Assets/PlayerHealth1.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class PlayerHealth1 : MonoBehaviour
+{
+    float healthMax = 100;
+    float curHealth = 100;
+    float takeDmg = 5;
+    float takeHealth = 10;
+
+    [SerializeField] TextMeshProUGUI healthText;
+
+    private void Start()
+    {
+        curHealth = healthMax;
+    }
+    void Update()
+    {
+        healthText.text = curHealth.ToString("F0");
+
+        if (curHealth <= 0)
+        {
+            curHealth = 0f;
+        }
+    }
+
+    void TakeDmg()
+    {
+        curHealth -= takeDmg;
+    }
+
+    void TakeHealth()
+    {
+        curHealth -= takeHealth;
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Zombie1")) //TODO(@Bryan) Use more modern function collision.gameobject.CompareTage("zombie1")
+        {
+            TakeDmg();
+        }
+        if (collision.gameObject.CompareTag("Zombie2"))
+        {
+            TakeDmg();
+        }
+        if (collision.gameObject.CompareTag("Zombie3"))
+        {
+            TakeDmg();
+        }
+        if (collision.gameObject.CompareTag("Zombie4"))
+        {
+            TakeDmg();
+        }
+        //if (collision.gameObject.CompareTag("health"))
+        //{
+        //    TakeHealth();
+        //}
+    }
+
+}

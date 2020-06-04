4ddc2686aba26115c428b4bd8b3821d319ca1da0
Bryan <bryancrim@gmail.com>
2020-06-04 12:49:05 +0200

    CheckWin script added

---
+++ Assets/Scripts/CheckWin.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CheckWin : MonoBehaviour
+{
+    private bool isOpen = false;
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("chest"))
+        {
+            isOpen = true;
+        }
+    }
+}

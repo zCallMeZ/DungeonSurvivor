16cf0e3ca2e3121d38a45fa7a8479a27609c2330
Bryan <bryancrim@gmail.com>
2020-06-07 19:16:53 +0200

    3 scripts for A* added

---
+++ Assets/Scripts/World/Astar/Node.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Node
+{
+	public bool canWalk;
+	public Vector3 worldPos;
+	public int gridX;
+	public int gridY;
+
+	public int gCost;
+	public int hCost;
+	public Node parent;
+
+	public Node (bool _canWalk, Vector3 _worldPos, int _gridX, int _gridY)
+	{
+		canWalk = _canWalk;
+		worldPos = _worldPos;
+		gridX = _gridX;
+		gridY = _gridY;
+	}
+
+	public int fCost
+	{
+		get
+		{
+			return gCost + hCost;
+		}
+	}
+}
